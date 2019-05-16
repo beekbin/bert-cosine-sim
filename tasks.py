7dad8693aacf6f96e9a5a80bbae87a25a1eebe63
Songbin Liu <songbin@fb.com>
2019-05-15 23:50:39 -0400

    remove unnecessary code

--- tasks.py
+++ tasks.py
@@ -372,45 +372,6 @@ class WnliProcessor(DataProcessor):
         return examples
 
 
-class FeedProcessor(DataProcessor):
-    """Processor for the Feeds data set."""
-
-    def get_train_examples(self, data_dir):
-        """See base class."""
-        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.tsv")))
-        return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
-
-    def get_dev_examples(self, data_dir):
-        """See base class."""
-        return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
-
-    def get_test_examples(self, data_dir):
-        """See base class."""
-        return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
-
-    def get_labels(self):
-        """See base class."""
-        return [None]
-
-    def _create_examples(self, lines, set_type):
-        """Creates examples for the training and dev sets."""
-        examples = []
-        for (i, line) in enumerate(lines):
-            if i == 0:
-                continue
-            # guid = "%s-%s" % (set_type, i)
-            guid = line[0]
-            label = float(line[1]) / 3.0
-            text_a = line[2]
-            text_b = line[3]
-            examples.append(
-                InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
-        return examples
-
-
 def convert_examples_to_features(examples, label_list, max_seq_length,
                                  tokenizer, output_mode):
     """Loads a data file into a list of `InputBatch`s."""
